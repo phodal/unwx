f5bc62dc8b620485d521cfa8943ee459e315c457
Fengda HUANG <h@phodal.com>
2016-09-23 16:03:05 +0800

    Transform ing

--- pack.js
+++ pack.js
@@ -1,15 +1,19 @@
 "use strict";
 function init() {
-    var glob = require("glob"), fs = require("fs"), path = require("path"), log = { info: console.log};
+    var glob = require("glob"), fs = require("fs"), path = require("path"), log = {
+        info: console.log,
+        error: console.log
+    };
 
-    pack = function(projectPath, zipPath, callback) {
+    pack = function (projectPath, zipPath, callback) {
         var c = 0, o = 1, a = 2, B = 3, w = 4, packetHead = [new Buffer(1), new Buffer(4), new Buffer(4), new Buffer(4), new Buffer(1)];
         packetHead[c].writeIntLE(190), packetHead[o].writeInt32BE(1), packetHead[w].writeIntLE(237);
 
-        var fileNumber = 0, v = [], h = [], relativePaths = [], fileDatas = [], E = [];
-        glob(projectPath + "/**", {nodir: !0}, function(size, files) {
-            size ? log.error(size) : !function() {
-                fileNumber = files.length, files.forEach(function(file) {
+        var fileNumber = 0, pathWithPacketHeadBuffer = [], pathWithPacketHead = [], relativePaths = [], fileDatas = [], fileDatasBuffer = [];
+        glob(projectPath + "/**", {nodir: !0}, function (size, files) {
+            size ? log.error(size) : !function () {
+                fileNumber = files.length;
+                files.forEach(function (file) {
                     var data = fs.readFileSync(file),
                         relativePath = path.relative(projectPath, file),
                         pathWithReplace = new Buffer("/" + relativePath.replace(/\\/g, "/"));
@@ -19,27 +23,27 @@ function init() {
                 });
 
                 var packetLength = 18 + 12 * fileNumber + Buffer.concat(relativePaths).length;
-                h = relativePaths.map(function(r, n) {
-                    console.log(r, n);
+                pathWithPacketHead = relativePaths.map(function (path, n) {
                     var t = new Buffer(4);
-                    t.writeInt32BE(r.length);
+                    t.writeInt32BE(path.length);
                     var f = new Buffer(4), u = fileDatas[n].length, i = packetLength;
                     f.writeInt32BE(i), packetLength += u;
                     var c = new Buffer(4);
-                    return c.writeInt32BE(u), Buffer.concat([t, r, f, c])
+                    return c.writeInt32BE(u), Buffer.concat([t, path, f, c])
                 });
 
-                var o = new Buffer(4);
-                o.writeInt32BE(fileNumber);
-                h.unshift(o);
-                v = Buffer.concat(h);
-                E = Buffer.concat(fileDatas);
-                packetHead[a].writeInt32BE(v.length);
-                packetHead[B].writeInt32BE(E.length);
+                var filesNumberBuffer = new Buffer(4);
+                filesNumberBuffer.writeInt32BE(fileNumber);
+                pathWithPacketHead.unshift(filesNumberBuffer);
+
+                pathWithPacketHeadBuffer = Buffer.concat(pathWithPacketHead);
+                fileDatasBuffer = Buffer.concat(fileDatas);
+
+                packetHead[a].writeInt32BE(pathWithPacketHeadBuffer.length);
+                packetHead[B].writeInt32BE(fileDatasBuffer.length);
                 packetHead = Buffer.concat(packetHead);
 
-                var zipData = Buffer.concat([packetHead, v, E]);
-                console.log(zipData);
+                var zipData = Buffer.concat([packetHead, pathWithPacketHeadBuffer, fileDatasBuffer]);
                 fs.writeFileSync(zipPath, zipData);
                 log.info("pack.js create " + zipPath + " success!");
                 callback(null, zipPath);
--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 var pack = require("./pack");
 
+console.log("--------------------------start--------------------------");
 pack("/Users/fdhuang/learing/wx/test/", "/Users/fdhuang/learing/wx/output.wx", function(a, zipPath){
+	console.log("--------------------------output--------------------------");
 	console.log(zipPath);
 });
