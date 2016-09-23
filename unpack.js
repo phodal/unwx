8ec8b94899457b05fb0f60a46b57a52c9976bcaa
Fengda HUANG <h@phodal.com>
2016-09-24 07:45:45 +0800

    Try to reslove unpack issue

--- unpack.js
+++ unpack.js
@@ -6,16 +6,19 @@ fs.readFile("./output.wx", function (err, data) {
     var filePathsBuffer = data.slice(18, 18 + pathHeadLength);
     var allFilesBufferLength = data.readInt32BE(9, 13);
 
-
     var bufferLength = 14 + pathHeadLength;
     var fileStartPos = 0;
     var bufferSize = 4;
+    var lastDataLength = 0;
     while (bufferLength < allFilesBufferLength) {
         var fileLength = filePathsBuffer.readInt32BE(fileStartPos, fileStartPos + bufferSize);
         var filePath = filePathsBuffer.slice(fileStartPos + bufferSize, fileStartPos + bufferSize + fileLength);
         var fileDataLength = filePathsBuffer.readInt32BE(fileLength + bufferSize * 2, fileLength + bufferSize * 3);
-        var fileData = data.slice(14 + fileStartPos + pathHeadLength, 14 + fileStartPos + pathHeadLength + fileDataLength);
+        var fileData = data.slice(14 + pathHeadLength + lastDataLength, 14 + pathHeadLength + fileDataLength);
 
+        lastDataLength = fileDataLength;
+
+        console.log(fileData.toString());
         fs.writeFile('.' + filePath, fileData, function(err) {
             if(err) {
                 return console.log(err);
