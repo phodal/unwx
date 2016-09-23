2a6cd8cb919d593a8f49ebefbb222e4c5ccb51e2
Fengda HUANG <h@phodal.com>
2016-09-23 20:33:44 +0800

    add interration for path

--- unpack.js
+++ unpack.js
@@ -3,32 +3,41 @@ var lodash = require("lodash");
 
 fs.readFile("./output.wx", function (err, data) {
     var pathHeadLength = data.readInt32BE(5, 9);
+    var filePathsBuffer= data.slice(18, 18 + pathHeadLength);
     var allFilesBufferLength = data.readInt32BE(9, 13);
-
-    var pathWithPacketHeadBuffer = data.slice(13, 13 + pathHeadLength);
-
-    var fileNumbers = data.readInt32BE(14, 18);
-
-    var firstFilesLength = data.readInt32BE(18, 22);
-    var firstFilePath = data.slice(22, 22 + firstFilesLength);
-    var currentBuffer = data.readInt32BE(22 + firstFilesLength, 26 + firstFilesLength);
-    var fileDataLengthBuffer = data.readInt32BE(22 + firstFilesLength + 4, 22 + firstFilesLength + 8);
-
-    console.log(firstFilePath.toString());
-    console.log(currentBuffer);
-    console.log(data.slice(14 + pathHeadLength, 14 + pathHeadLength + fileDataLengthBuffer).toString());
-
-    // var bufferLength = 14 + pathHeadLength;
-    // var fileStartPos = 14;
-    // var bufferSize = 4;
-    // while(bufferLength < allFilesBufferLength) {
-    //     var fileLength = data.readInt32BE(fileStartPos + bufferSize, fileStartPos + bufferSize * 2);
-    //     var filePath = data.slice(fileStartPos + bufferSize * 2, fileStartPos + bufferSize  * 2 + fileLength);
+    // var pathWithPacketHeadBuffer = data.slice(13, 13 + pathHeadLength);
+    // var fileNumbers = data.readInt32BE(14, 18);
     //
-    //     var fileDataLength = data.readInt32BE(fileStartPos + bufferSize * 3 + fileLength, fileStartPos  + bufferSize * 4 + fileLength);
-    //     var fileData = data.slice(fileStartPos + pathHeadLength, fileStartPos + pathHeadLength + fileDataLength).toString();
     //
-    //     console.log("--------------------------------");
-    //     console.log(filePath.toString());
-    // }
+    // var firstFilesLength = filePathsBuffer.readInt32BE(0, 4);
+    // var firstFilePath = filePathsBuffer.slice(4, 4 + firstFilesLength);
+    // var fileDataLengthBuffer = filePathsBuffer.readInt32BE(8 + firstFilesLength , 8 + firstFilesLength + 4);
+    //
+    // console.log(firstFilesLength);
+    // console.log(fileDataLengthBuffer);
+    // console.log(data.slice(14 + pathHeadLength, 14 + pathHeadLength + fileDataLengthBuffer).toString());
+    //
+    // var firstBufferLenght = 12 + firstFilesLength;
+    //
+    // var firstFilesLength = filePathsBuffer.readInt32BE(firstBufferLenght, firstBufferLenght + 4);
+    // var firstFilePath = filePathsBuffer.slice(firstBufferLenght + 4, firstBufferLenght + 4 + firstFilesLength);
+    //
+    // console.log(firstFilesLength);
+    // console.log(fileDataLengthBuffer);
+
+    var bufferLength = 14 + pathHeadLength;
+    var fileStartPos = 0;
+    var bufferSize = 4;
+    while(bufferLength < allFilesBufferLength) {
+        var fileLength = filePathsBuffer.readInt32BE(fileStartPos, fileStartPos + bufferSize);
+        var filePath = filePathsBuffer.slice(fileStartPos + bufferSize, fileStartPos + bufferSize + fileLength);
+
+        var fileDataLength = data.readInt32BE(fileStartPos + bufferSize * 2 + fileLength, fileStartPos  + bufferSize * 3 + fileLength);
+        var fileData = filePathsBuffer.slice(fileStartPos + pathHeadLength, fileStartPos + pathHeadLength + fileDataLength).toString();
+
+        console.log(filePath.toString());
+        console.log(fileData);
+        fileStartPos = bufferSize * 3 + fileLength + fileStartPos;
+        bufferLength = bufferLength + fileStartPos;
+    }
 });
